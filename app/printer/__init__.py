a82525260cda8d57d2afa1df29d816068c0d4a18
Jack Serrino <Jserrino@gmail.com>
2016-02-01 05:09:36 -0500

    Print raffle first

--- app/printer/__init__.py
+++ app/printer/__init__.py
@@ -6,7 +6,7 @@ from os.path import join, dirname
 def print_user(user):
     printer = os.environ.get('PRINTER_NAME')
     label = join(dirname(__file__), 'labels', 'xfair.png')
-    image.create_image(user)
-    os.system('lpr -P "{:s}" {:s}'.format(printer, label))
     image.create_image(user, raffle=True)
+    os.system('lpr -P "{:s}" {:s}'.format(printer, label))
+    image.create_image(user)
     os.system('lpr -P "{:s}" {:s}'.format(printer, label))
