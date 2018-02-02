0eff36dd5df17babea5ce2eae0d523d84ec2865e
Moin Nadeem <mnadeem@mit.edu>
2018-02-02 18:03:11 -0500

    updating printer version

--- runserver.py
+++ runserver.py
@@ -1,7 +1,7 @@
 from checkin import app
 
 app.config.update(
-    PRINTER_NAME="LabelWriter-450-Turbo",
+    PRINTER_NAME="DYMO_LabelWriter-450",
     LABEL_WIDTH=760,
     LABEL_HEIGHT=410,
     debug=True
